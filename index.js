ea2229c20cc66af2cc631c355328efcd6d22bec8
Emily Evans <emilyevans@wifi-roaming-128-4-107-193.host.udel.edu>
2020-02-18 18:44:32 -0500

    reversed name!!!!! bam what!

--- index.js
+++ index.js
@@ -4,11 +4,26 @@ $(function () {
 
 	$("#nameform").on('submit', function (e) {
 		e.preventDefault();
-		var fname = $('#fname').reverse().val();
+		var fname = $('#fname').val();
 		var lname = $('#lname').val();
 		var newHTML = '<h1>' + fname + ' ' + lname + '</h1>';
-		$(".jumbotron").removeClass('d-none').html(newHTML);
+
+		var reversedfname = stringReverse(fname);
+		var reversedlname = stringReverse(lname);
+		var newHTMLReverse = '<h1>' + reversedfname + ' ' + reversedlname + '</h1>';
+
+		// $(".jumbotron").removeClass('d-none').html(newHTML);
+		$(".jumbotron").removeClass('d-none').html(newHTMLReverse);
 		return false;  //prevents reload on submit
 	});
 });
-$($("li").get().reverse()).each(function() { /* ... */ });
+
+// code found on codepen.io https://codepen.io/charlessipe/pen/vyVezx
+function stringReverse(string) {
+	var splitString = string.split("");
+	console.log(splitString);
+	var reverseString = splitString.reverse();
+	console.log(reverseString);
+	var joinString = reverseString.join("");
+	return joinString;
+} // end function
